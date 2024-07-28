421e5a14aec2cda496eb5a94a2499e2891aebe19
梦辉 <38788738+songsiqiu@users.noreply.github.com>
2024-07-28 16:55:27 +0800

    feature: 我的页面完成

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { getActiveList } from '@/api/active/list'
+import { ActiveListResp } from '@/api/active/list/types'
 import useUserState from '@/store/userState'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const notice = ref(false)
 /** 用户基本信息 */
@@ -9,6 +11,15 @@ const userBaseInfo = useUserState().getUserBaseInfo
 /** tab */
 const activeTab = ref(0)
 
+/** 获取活动列表 */
+const activeList = ref<ActiveListResp[]>([])
+const getActivityList = async () => {
+  const { data } = await getActiveList()
+  activeList.value = data
+}
+onMounted(() => {
+  getActivityList()
+})
 console.log(userBaseInfo)
 </script>
 
@@ -19,7 +30,7 @@ console.log(userBaseInfo)
       <img v-if="notice" class="w-[20px] h-[24px]" src="@/static/notice-nomsg.png" alt="" />
       <img v-else class="w-[20px] h-[24px]" src="@/static/notice-msg.png" alt="" />
     </view>
-    <view class="flex-1 overflow-y-auto">
+    <view>
       <view v-if="userBaseInfo" class="user-info-panel p-[12px] flex flex-col items-center justify-center">
         <img v-if="userBaseInfo.avatar" class="w-[100px] h-[100px] rounded-full" :src="userBaseInfo.avatar" alt="" />
         <img v-else class="w-[100px] h-[100px] rounded-full" src="@/static/defaultAvatar.png" alt="" />
@@ -31,11 +42,44 @@ console.log(userBaseInfo)
         <view class="tab-item" :class="activeTab === 0 ? 'active-tab-item' : ''" @click="activeTab = 0">我报名的活动</view>
         <view class="tab-item" :class="activeTab === 1 ? 'active-tab-item' : ''" @click="activeTab = 1">我投票的活动</view>
       </view>
+      <view class="flex-1 overflow-y-auto">
+        <view class="active-item h-[178px] p-3" v-for="(item, index) in activeList" :key="index">
+          <view class="content flex gap-2">
+            <view class="left relative">
+              <view class="number absolute top-0 left-0 bg-[#FF5900] w-[78px] h-[25px] leading-[25px] text-[14px] text-center text-white">
+                {{ item.status }}
+              </view>
+              <img class="w-[156px]" src="@/static/active-default.png" alt="" />
+            </view>
+            <view class="right">
+              <view class="top h-full flex flex-col justify-between">
+                <view class="title font-bold line-clamp-1 whitespace-nowrap text-ellipsis"> {{ item.title }}</view>
+                <view class="time">
+                  <view class="start-time time">{{ item.startTime }}</view>
+                  <view class="end-time time">{{ item.endTime }}</view>
+                </view>
+                <view class="number flex gap-[3px] items-center">
+                  <img src="@/static/fire.png" class="w-[12px] h-[16px] mr-1" alt="" srcset="" />
+                  <span class="text-[#FF5900] line-clamp-1 text-nowrap text-[14px] leading-[18px]">{{ item.personNum }}</span>
+                </view>
+              </view>
+            </view>
+          </view>
+          <view class="bottom flex items-center gap-3 mt-2">
+            <van-button type="primary" plain size="small">关闭</van-button>
+            <van-button type="primary" size="small">转发投票</van-button>
+            <van-button type="warning" size="small">投票详情</van-button>
+          </view>
+        </view>
+      </view>
     </view>
   </view>
 </template>
 
 <style lang="scss" scoped>
+.number {
+  border-radius: 5px 0 5px 0;
+}
 .divider {
   width: 100%;
   border: 1px solid rgba(241, 236, 236, 0.3);
@@ -51,5 +95,14 @@ console.log(userBaseInfo)
   padding: 6px;
   border-radius: 5px;
   background-color: #f1f1f1;
+}
+.bottom {
+  :deep(.van-button) {
+    flex: 1;
+  }
+}
+.time {
+  color: #999999;
+  font-size: 14px;
 }
 </style>
